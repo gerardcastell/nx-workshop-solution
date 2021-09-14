7f427f3f09762798e37e8f520ac0612386f1be3a
Gerard Cleeverea <gerard.castell@cleverea.com>
2021-09-14 18:22:45 +0200

    [ADD] component shared ui

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MatCardModule],
+  imports: [BrowserModule, MatCardModule, StoreUiSharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
