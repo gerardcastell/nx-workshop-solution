57442857f785eba6b7420334a4cb7bc9e459931c
Gerard Cleeverea <gerard.castell@cleverea.com>
2021-09-14 20:00:14 +0200

    [ADD] feature module

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
-
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MatCardModule, StoreUiSharedModule],
+  imports: [
+    BrowserModule,
+    MatCardModule,
+    StoreUiSharedModule,
+    RouterModule.forRoot([
+      {
+        path: 'game/:id',
+        loadChildren: () =>
+          import('@bg-hoard/store/feature-game-detail').then(
+            (module) => module.StoreFeatureGameDetailModule
+          ),
+      },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
