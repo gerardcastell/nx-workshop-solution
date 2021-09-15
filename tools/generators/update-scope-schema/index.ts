2326be2a25f4b6c34caf74437a064be267223da1
Gerard Cleeverea <gerard.castell@cleverea.com>
2021-09-15 19:28:09 +0200

    [ADD] generator to automaticale update scopes

---
+++ tools/generators/update-scope-schema/index.ts
@@ -0,0 +1,48 @@
+import {
+  Tree,
+  formatFiles,
+  installPackagesTask,
+  updateJson,
+} from '@nrwl/devkit';
+import { libraryGenerator } from '@nrwl/workspace/generators';
+// import { updateJson } from '@nrwl/devkit';
+import { readJson } from '@nrwl/devkit';
+export default async function (tree: Tree, schema: any) {
+  // await libraryGenerator(tree, { name: schema.name });
+  // await formatFiles(tree);
+  function getScopes(nxJson: any) {
+    const projects: any[] = Object.values(nxJson.projects);
+    const allScopes: string[] = projects
+      .map((project) =>
+        project.tags
+          // take only those that point to scope
+          .filter((tag: string) => tag.startsWith('scope:'))
+      )
+      // flatten the array
+      .reduce((acc, tags) => [...acc, ...tags], [])
+      // remove prefix `scope:`
+      .map((scope: string) => scope.slice(6));
+    // remove duplicates
+    return Array.from(new Set(allScopes));
+  }
+  const scopeList = await getScopes(readJson(tree, 'nx.json'));
+
+  await updateJson(tree, 'tools/generators/util-lib/schema.json', (obj) => {
+    return {
+      ...obj,
+      properties: {
+        ...obj.properties,
+        directory: {
+          ...obj.properties.directory,
+          items: scopeList.map((scope) => ({ value: scope, label: scope })),
+        },
+      },
+    };
+  });
+  // await updateJson(tree, 'workspace.json', (obj) => {
+  //   return { ...obj, defaultProject: 'api' };
+  // });
+  return () => {
+    installPackagesTask(tree);
+  };
+}
